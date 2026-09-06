1bee78109a9e5206683bd7e651b6a7047245e1bc
KAWTARMIO <bestnaji89@gmail.com>
2026-09-06 23:53:41 +0100

    update

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,6 +4,15 @@ int main(){
 for(int i = 100 ; i <= 999; i++){
     for(int k = 100; k <= 999; k++){
 long long product = i * k;
+long long original = product;
+long long reversed = 0;
+
+while(product != 0){
+    int digit = product % 10;
+    reversed = reversed * 10 + digit;
+    product = product / 10;
+  }
+  for( int largest = 0)
 
     }
 }
