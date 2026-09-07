25009ebb59fefc30c96304f305538183bb228ba8
KAWTARMIO <bestnaji89@gmail.com>
2026-09-07 22:54:39 +0100

    update

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
+    long long largest = 0;
 for(int i = 100 ; i <= 999; i++){
     for(int k = 100; k <= 999; k++){
 long long product = i * k;
@@ -12,9 +13,13 @@ while(product != 0){
     reversed = reversed * 10 + digit;
     product = product / 10;
   }
-  for( int largest = 0)
-
+    if(original == reversed){
+        if(original > largest){
+            largest = original;
+        }
     }
+  }
 }
+cout<<"the largest palindrome product of two 3-digit numbers is:"<<largest<<endl;
 return 0;
 }
