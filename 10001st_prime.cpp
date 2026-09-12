e7d6690f40c7792f501d94205c82e7e963031e3f
KAWTARMIO <bestnaji89@gmail.com>
2026-09-12 23:54:37 +0100

    update

--- 10001st_prime.cpp
+++ 10001st_prime.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
 using namespace std;
 int main(){
- int n = 0;
-}
+    int get = 0;
+    int num =1;
+    int prime = 0;
+
+    while(get < 10001){
+        num++;
+        bool isprime = true;
+    for(int i = 2; i <= num/2; i++){
+        if (num % i == 0){
+            isprime = false;
+            break;
+        }
+        if(isprime){
+            prime = num;
+            get++;
+
+        }
+        
+    }
+    
+cout<<"the 10001st prime number is:"<<prime<<endl;
+ }
+ return 0;
+ }
